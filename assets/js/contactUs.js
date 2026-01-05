3fbb8b80e257405fcd58a285dcf223fc844cdf4e
Leon <bnq1798@gmail.com>
2026-01-05 08:27:45 +0800

    v1.0

--- assets/js/contactUs.js
+++ assets/js/contactUs.js
@@ -25,10 +25,29 @@ const contactUsApp = {
                             ],
         mainChallenges:'',
         desiredGoal:'',
-        // 
+        //聯絡偏好
+        preferredContactMethod:'信箱',
+        preferredContactMethodOption:['信箱', '電話', 'LINE', 'Discord'],
+        preferredContactTime:[],
+        preferredContactTimeOption:[
+                                     '平日上午(9:00-12:00)', '平日下午(13:00-17:00)', '平日晚上(18:00-21:00)',
+                                     '週末上午(9:00-12:00)', '週末下午(13:00-17:00)', '週末晚上(18:00-21:00)'
+                                    ],
+        findOutThroughAChannel:[],
+        findOutThroughAChannelOption:[
+                                      'Google 搜尋', 'Instagram', 'Facebook', 'Threads', 'YouTube',
+                                      'Podcast / 節目推薦', '朋友或同事推薦', '曾參加職旅活動 / 工作坊', '職涯顧問介紹',
+                                      '其他（請填寫下題）'
+                                    ],
+
       },
       employmentStatusBtn: false,
-      expectExpertStatusBtn: false
+      expectExpertStatusBtn: false,
+
+      //checkSwitch
+      acceptNewDataCheckedText: '',
+      acceptNewDataChecked: false,
+      acceptNewDataShowSwitch: false
     }
   },
   watch:{
@@ -51,13 +70,31 @@ const contactUsApp = {
    'defaultGuest.desiredGoal'(val){
     console.log(val);
    },
+   'defaultGuest.preferredContactMethod'(val){
+    console.log(val);
+   },
+   'defaultGuest.preferredContactTime'(val){
+    console.log(val);
+   },
+   'defaultGuest.findOutThroughAChannel'(val){
+    console.log(val);
+   },
+
+   //watch-remaid-check (V-model 也需要此值, 才可以讀取到)
+   acceptNewDataChecked(newValue){ 
+    if(newValue === true){
+      this.acceptNewDataShowSwitch = !this.acceptNewDataShowSwitch;
+      this.acceptNewDataCheckedText = '';
+    }
+   },
+
   },
   computed:{ 
   // 利用 computed 功能, 只要this.defaultGuest.consultationTopicOption 有新增選項就自動計算並取出單數的索引值
   // 並將 consultationTopicKey() 函式直接放到 template 用 ; 也就是 template 用頂層 computed，不要再去 data 裡的空陣列
     consultationTopicKey(){
       return this.defaultGuest.consultationTopicOption.map((item,key)=>key).filter((key)=> key % 2 === 1);
-    }
+    },
   //修改前
   // consultationTopicKey() {
       //   //更原始
@@ -79,7 +116,8 @@ const contactUsApp = {
       //   //    return key % 2 === 1 ;
       //   //  })
       //  }  
-  },
+  
+    },
   methods:{
     //目前身分
     employmentStatus(e){
@@ -96,8 +134,57 @@ const contactUsApp = {
     expectExpertValue(data){
       this.defaultGuest.expectExpert = data;
       this.expectExpertStatus();
+    },
+    //聯絡時段
+    //最符合 Vue 的「關注點分離」
+    contactTimeClass(key){
+      return {
+        'mb-xl-0': ![0, 1, 2].includes(key),
+        'mb-2': key !== 5
+      }
+    },
+    //得知我們
+    ThroughAChannelClass(key){
+      return {
+        'me-lg-5': ![4,8,9].includes(key),
+        'me-lg-0': [8].includes(key),
+        'me-md-4': [1,5,6,7].includes(key),
+        'me-sm-4': [1,5,6,7].includes(key),
+        'me-4': ![1,4,6,7,9].includes(key),
+        'mb-lg-2':[8].includes(key),
+        'mb-2':![8,9].includes(key),
+        'mb-0':[8,9].includes(key)
+      }
+    },
+
+    //btn
+    sentContactBtn(){
+
+     const sentContactErrorValue = [
+      {
+        test:()=>{
+          return !this.acceptNewDataChecked // true
+        },
+        acceptNewDataCheckedErrorMessage:'請先勾選接收職旅最新資訊和專業內容'
+      },
+     ];
+
+     // Array.prototype.find() 的行為是： 「幫我找第一個 test() 回傳 true 的規則」
+     const findTestError = sentContactErrorValue.find((item)=>item.test());
+     // 有找到符合條件->那個物件
+     // 沒找到->undefined
+
+     console.log(findTestError);
+     
+     if(findTestError){ // 如果有篩出 error
+      if(findTestError.acceptNewDataCheckedErrorMessage){
+        this.acceptNewDataShowSwitch = true;
+        this.acceptNewDataCheckedText = findTestError.acceptNewDataCheckedErrorMessage;
+        }
+      }
+     
+
     }
-    
   }
 }
 createApp(contactUsApp).mount(contactUs);
